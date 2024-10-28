00093cc53d86447c3966666549e8a906316a7dc0
Łukasz Matuszewski <lucas.matuszewski@gmail.com>
2024-10-28 23:13:56 +0000

    Manually Added - edukey red color

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,18 @@ const config: Config = {
       colors: {
         background: 'var(--background)',
         foreground: 'var(--foreground)',
-        red: 'var(--red)', // Add the company red color
+        red: {
+          100: 'var(--red-100)',
+          200: 'var(--red-200)',
+          300: 'var(--red-300)',
+          DEFAULT: 'var(--red)',
+          400: 'var(--red-400)',
+          500: 'var(--red-500)',
+          600: 'var(--red-600)',
+          700: 'var(--red-700)',
+          800: 'var(--red-800)',
+          900: 'var(--red-900)',
+        },
       },
     },
   },
